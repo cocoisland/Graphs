4de7049b76a8f6cc65053b73c30d6890a24eb184
Tony Tia <webmaster@tonytia.com>
2019-06-26 15:54:30 -0700

    BFS and DFS traverse

--- projects/graph_problem/island.py
+++ projects/graph_problem/island.py
@@ -15,9 +15,9 @@ island_counter(islands) # returns 4
 
 islands = [[0, 1, 0, 1, 0],
            [1, 1, 0, 1, 1],
-           [0, 0, 1, 0, 0],
+           [0, 0, 0, 0, 0],
            [1, 0, 1, 0, 0],
-           [1, 1, 0, 0, 0]]
+           [1, 1, 0, 1, 1]]
 
 # islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1],
 #            [0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
@@ -33,6 +33,16 @@ islands = [[0, 1, 0, 1, 0],
 
 
 # 1. Translate problem into graph terminology:
+'''
+Understand the problem:
+1. An island is made up of at least 3 ones' located in 3 of the 4 direction(n,s,w,e).
+Island boundary is marked by 0 or matrix edge.
+Is a graph problem because of keyword "connected" to north, south, west or east.
+
+2. Build a graph based on "connected" to north, south, west, or east.
+
+3. Traverse graph checking for connection to north, south, west or east.
+'''
 
 
 
@@ -74,12 +84,26 @@ class Stack():
     def size(self):
         return len(self.stack)
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 
 def island_counter(matrix):
+    # DFS - traverse vertically by each row first, then horizontally by column
     # Create a visited matrix
     visited = []
-    for i in range(len(matrix)):
-        visited.append([False] * len(matrix[0]))
+    for i in range(len(matrix)): # number of rows in 2x2 matrix
+        visited.append([False] * len(matrix[0])) # number of columns in 2x2 matrix
     island_count = 0
     # Walk through each cel in the matrix
     for col in range(len(matrix[0])):
@@ -95,7 +119,27 @@ def island_counter(matrix):
     # Return island count
     return island_count
 
+def island_counter_BFS(matrix):
+    # BFS - traverse horizontal by each column first, then vertically by row
+    # Create a visited matrix
+    visited = []
+    for i in range(len(matrix)): # number of rows in 2x2 matrix
+        visited.append([False] * len(matrix[0])) # number of columns in 2x2 matrix
+    island_count=0
+    # Walk through each cel in the matrix
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            # If that cel has not been visited
+            if not visited[row][col]:
+                # When I reach a 1,
+                if matrix[row][col] == 1:
+                    # Do a BFT and mark each as visited
+                    visited = bft(col, row, matrix, visited)
+                    # Then increment the counter by 1
+                    island_count += 1
 
+    # Return island count
+    return island_count
 
 def dft(col, row, matrix, visited):
     s = Stack()
@@ -111,5 +155,20 @@ def dft(col, row, matrix, visited):
     return visited
 
 
+def bft(col, row, matrix, visited):
+    s = Queue()
+    s.enqueue((col, row))
+    while s.size() > 0:
+        v = s.dequeue()
+        col = v[0]
+        row = v[1]
+        if not visited[row][col]:
+            visited[row][col] = True
+            for neighbor in get_neighbors(v, matrix):  # STUB
+                s.enqueue(neighbor)
+    return visited
 
-print(island_counter(islands))
+
+
+print(f' DFS-traverse vertically row, then horizontally column = {island_counter(islands)}')
+print(f' BFS-traverse horizontally column, then vertically row = {island_counter_BFS(islands)}')
