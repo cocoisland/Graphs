bf192fdc81b9a986705897af88150f2397b4a664
Tony Tia <webmaster@tonytia.com>
2019-06-27 16:48:59 -0700

    changing direction order make no difference to num of steps

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -69,6 +69,7 @@ def toEndNode():
         prevRoom=curRoom
         curRoom = player.currentRoom.id
         exits = player.currentRoom.getExits()
+        #exits.sort()   # changing the order of direction to east first, make no difference to num of steps.
         addGraph(prevRoom, curRoom, direction, exits)
     return direction
 
@@ -126,6 +127,9 @@ def checkAllExit():
 
 curRoom = player.currentRoom.id
 exits = player.currentRoom.getExits()
+#exits=sorted(exits) # changing direction east first, make no difference to num of 1004 steps.
+# print(f' exit = {exits} ')
+# input('enter')
 addGraph(None, curRoom,None, exits)
 
 while checkAllExit():
